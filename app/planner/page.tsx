c32579c6ddb4c17849b953e1c7010c53a5b3e4cd
fjain <fjralph200@gmail.com>
2024-08-28 14:09:44 -0400

    initialize files

---
+++ app/planner/page.tsx
@@ -0,0 +1,8 @@
+export default function Planner() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>This is the planner page</div>
+      </main>
+    );
+  }
+  
--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Shopping() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>This is the home page</div>
+      <div>This is the shopping page</div>
     </main>
   );
 }
