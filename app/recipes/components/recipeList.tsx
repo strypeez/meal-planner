502d2abc7623d4f2bec0f46a4be37f6ac78dd72d
fjain <fjralph200@gmail.com>
2024-09-03 14:15:56 -0400

    update recipe list

--- app/recipes/components/recipeList.tsx
+++ app/recipes/components/recipeList.tsx
@@ -6,8 +6,14 @@ type RecipeListProps = {
 }
 
 export default function RecipeList({recipe}: RecipeListProps) {
+    const [showExtra, setShowExtra] = useState(false);
+    console.log('this is recipe', recipe)
     return <div className="border border-black my-2">
-        <div className="border-b border-black p-4">{recipe.name}</div>
-        <div className="p-4"><h2>Ingredients</h2></div>
+        <div onClick={()=>{setShowExtra(!showExtra)}} className={`${showExtra ? 'border-b' : ''} border-black p-4`}>{recipe.name}</div>
+        <div className={`p-4 flex flex-col ${showExtra ? '': 'hidden'}`}>
+            <h2 className="font-bold">Ingredients</h2>
+            <div>{recipe.ingredients.map((ingredient, index) => (<div key={index}>{ingredient.quantity.number} {ingredient.quantity.measurement} {ingredient.name}</div>))}
+            </div>
+        </div>
     </div>
 }
